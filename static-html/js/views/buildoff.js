e0d309f4aee68a9edb840707faca929bd0aafbb7
coranos <coranos@users.noreply.github.com>
2021-04-30 21:06:12 -0500

    adding starfield

--- static-html/js/views/buildoff.js
+++ static-html/js/views/buildoff.js
@@ -1,4 +1,7 @@
 window.onLoad = () => {
+  document.getElementById('stars1').style.boxShadow = getBoxShadow(8*4*2);
+  document.getElementById('stars2').style.boxShadow = getBoxShadow(8*4);
+  document.getElementById('stars3').style.boxShadow = getBoxShadow(8);
   showAllProjects();
 };
 
@@ -14,4 +17,25 @@ window.showAllProjects = () => {
 window.showRegister = () => {
   document.querySelector('#registerButton').disabled = true;
   document.querySelector('#allProjectsButton').disabled = false; document.querySelector('#allProjects').className = 'display_none'; document.querySelector('#register').className = '';
+};
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const getBoxShadow = (count) => {
+  let boxShadow = '';
+  for (let i = 0; i < count; i++) {
+    const x = getRandomInt(0, 95);
+    const y = getRandomInt(0, 100);
+    if (i > 0) {
+      boxShadow += ',';
+    }
+    boxShadow += `${x}vw ${y}vh #FFF`;
+    boxShadow += ',';
+    boxShadow += `${x}vw ${y-95}vh #FFF`;
+  }
+  return boxShadow;
 };
